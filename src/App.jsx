e73291a4330da92be10a62f2df0c7a2e3a9d66bc
wi <diabloevagto@gmail.com>
2022-10-11 20:06:40 +0800

    add wagmi config and connect wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useConnect, useAccount, useDisconnect } from "wagmi";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { connect, connectors, error } = useConnect();
+  const { disconnect } = useDisconnect();
+  const { address, isConnected } = useAccount();
 
   return (
     <div className="App">
       <h1>Blockchain + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {isConnected ? (
+          <>
+            <p>{address}</p>
+            <button onClick={disconnect}>Disconnect</button>
+          </>
+        ) : (
+          connectors.map((connector) => (
+            <button key={connector.id} onClick={() => connect({ connector })}>
+              {connector.name}
+            </button>
+          ))
+        )}
+
+        {error && <p>{error.message}</p>}
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { chain, WagmiConfig, createClient } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+// Set up client
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new InjectedConnector({
+      chains: [chain.goerli],
+      options: {
+        name: "Injected",
+      },
+    }),
+  ],
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={client}>
+      <App />
+    </WagmiConfig>
   </React.StrictMode>
 );
