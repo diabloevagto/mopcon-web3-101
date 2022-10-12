60cb831bb44886a2710045d430cc96fc0bea8345
wi <diabloevagto@gmail.com>
2022-10-13 00:57:55 +0800

    add provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,27 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { chain, WagmiConfig, createClient } from "wagmi";
+import { chain, WagmiConfig, createClient, configureChains } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.goerli],
+  [publicProvider()]
+);
 
 // Set up client
 const client = createClient({
   autoConnect: true,
   connectors: [
     new InjectedConnector({
-      chains: [chain.goerli],
+      chains,
       options: {
         name: "Injected",
       },
     }),
   ],
+  provider,
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
